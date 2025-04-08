3a3eb35812d768ca8d5e897fa58f72e314f8398f
Pasang24 <pasangtheeng14@gmail.com>
2025-04-08 18:18:46 +0545

    feat: add login functionality in login form

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,16 +1,54 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { User } from "@/types";
 
 function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({
+    emailError: "",
+    passwordError: "",
+  });
+  const [validationError, setValidationError] = useState("");
+
+  const router = useRouter();
 
   const loginHandler = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
     // logic for handling login
+    setErrors({
+      emailError: "",
+      passwordError: "",
+    });
+    setValidationError("");
+
+    if (email.trim().length === 0) {
+      setErrors((prev) => ({ ...prev, emailError: "Email is required!" }));
+      return;
+    }
+    if (password.trim().length === 0) {
+      setErrors((prev) => ({
+        ...prev,
+        passwordError: "Password is required!",
+      }));
+      return;
+    }
+
+    const users: User[] = JSON.parse(localStorage.getItem("users") || "[]");
+
+    let userExists = users.some(
+      (user) => user.email === email && user.password === password
+    );
+
+    if (userExists) {
+      router.replace("/blogs");
+    } else {
+      setValidationError("Invalid Email or Password!");
+    }
   };
 
   return (
@@ -30,6 +68,9 @@ function LoginForm() {
           id="email"
           className="p-2 rounded-sm border-2 text-sm"
         />
+        {errors.emailError && (
+          <span className="text-xs text-red-400">{errors.emailError}</span>
+        )}
       </div>
       <div className="flex flex-col gap-2">
         <label htmlFor="password" className="font-semibold">
@@ -43,7 +84,13 @@ function LoginForm() {
           id="password"
           className="p-2 rounded-sm border-2 text-sm"
         />
+        {errors.passwordError && (
+          <span className="text-xs text-red-400">{errors.passwordError}</span>
+        )}
       </div>
+      {validationError && (
+        <span className="text-xs text-red-400">{validationError}</span>
+      )}
       <Button className="cursor-pointer">Login</Button>
       <p className="text-center">
         Don't have an account?{" "}
