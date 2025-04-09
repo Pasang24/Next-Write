cf9c2a163b4de30450940604c61ab8f73ce1e5cc
Pasang24 <pasangtheeng14@gmail.com>
2025-04-09 17:51:44 +0545

    feat: add functionality to navbar elements

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,6 +5,7 @@ import { Button, buttonVariants } from "./ui/button";
 import Container from "./Container";
 import ThemeToggleButton from "./ui/ThemeToggleButton";
 import { redirect } from "next/navigation";
+import { SquarePen, LogOut } from "lucide-react";
 
 interface NavbarProps {
   authenticatedRoute?: boolean;
@@ -18,25 +19,47 @@ function Navbar({ authenticatedRoute = false }: NavbarProps) {
   return (
     <nav>
       <Container className="flex justify-between items-center">
-        <Link href="/" className="font-bold text-2xl">
+        <Link
+          href={authenticatedRoute ? "/blogs" : "/"}
+          className="font-bold text-2xl"
+        >
           NextWrite
         </Link>
-        <div className="flex gap-2">
+        <div className="flex items-center gap-4">
           <ThemeToggleButton />
           {authenticatedRoute ? (
-            <>
-              <Link href="/add-blog">Write</Link>
-              <Button onClick={logoutHandler}>Logout</Button>
-            </>
+            <div className="flex">
+              <Link
+                href="/add-blog"
+                className="flex items-center gap-1 px-2 py-1.5 rounded-sm hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50"
+              >
+                <SquarePen size={18} />
+                Write
+              </Link>
+              <Button
+                className="text-base cursor-pointer font-normal"
+                variant={"ghost"}
+                onClick={logoutHandler}
+              >
+                <LogOut />
+                Logout
+              </Button>
+            </div>
           ) : (
-            <Link
-              href="/login"
-              className={`${buttonVariants({
-                variant: "default",
-              })}`}
-            >
-              Get Started
-            </Link>
+            <div className="flex">
+              <Link
+                href="/login"
+                className="flex items-center gap-1 px-2 py-1.5 rounded-sm hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50"
+              >
+                Login
+              </Link>
+              <Link
+                href="/signup"
+                className="flex items-center gap-1 px-2 py-1.5 rounded-sm hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50"
+              >
+                Signup
+              </Link>
+            </div>
           )}
         </div>
       </Container>
