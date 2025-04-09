d8404546465228bbb99c81437d9d914ad5c5cd78
Pasang24 <pasangtheeng14@gmail.com>
2025-04-09 09:37:51 +0545

    feat: implement authguard for public and protected routes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,17 +1,20 @@
+import AuthGuard from "@/components/AuthGuard";
 import Container from "@/components/Container";
 import LoginForm from "@/components/LoginForm";
 
 function Login() {
   return (
-    <Container className="flex flex-col items-center gap-10">
-      <h2 className="text-4xl md:text-5xl font-semibold mt-10 text-center">
-        Welcome Back
-      </h2>
-      <div className="flex flex-col gap-4 items-center w-full">
-        <p className="text-xl text-center">Please Login to continue</p>
-        <LoginForm />
-      </div>
-    </Container>
+    <AuthGuard>
+      <Container className="flex flex-col items-center gap-10">
+        <h2 className="text-4xl md:text-5xl font-semibold mt-10 text-center">
+          Welcome Back
+        </h2>
+        <div className="flex flex-col gap-4 items-center w-full">
+          <p className="text-xl text-center">Please Login to continue</p>
+          <LoginForm />
+        </div>
+      </Container>
+    </AuthGuard>
   );
 }
 
---
+++ components/AuthGuard.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { redirect } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+interface AuthGuardProps {
+  children: React.ReactNode;
+  authenticatedRoute?: boolean;
+}
+
+function AuthGuard({ children, authenticatedRoute = false }: AuthGuardProps) {
+  const [showPage, setShowPage] = useState(false);
+  useEffect(() => {
+    const isLoggedIn: boolean = JSON.parse(
+      localStorage.getItem("isLoggedIn") || "false"
+    );
+
+    // if the route requires authentication but the user is not logged in
+    if (authenticatedRoute && !isLoggedIn) {
+      // we redirect them to login page in order to authenticate them
+      return redirect("/login");
+    }
+    //if the route doesn't require authentication but the user is logged in
+    else if (!authenticatedRoute && isLoggedIn) {
+      // we redirect them to blogs page
+      return redirect("/blogs");
+    }
+    // allow the page to be shown
+    else {
+      setShowPage(true);
+    }
+  }, []);
+
+  if (!showPage) return null;
+
+  return <>{children}</>;
+}
+
+export default AuthGuard;
