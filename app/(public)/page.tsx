5725b1250ccee3a27c5ec7b47df667b43e7b9d57
Pasang24 <pasangtheeng14@gmail.com>
2025-04-11 19:49:07 +0545

    style: make home page responsive

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -7,9 +7,11 @@ import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <Container className="flex items-center h-screen">
+    <Container className="flex items-center mt-44">
       <div className="flex flex-col gap-10">
-        <h2 className="text-7xl font-semibold">Your Stories Stored Safely</h2>
+        <h2 className="text-4xl xs:text-5xl sm:text-7xl font-semibold">
+          Your Stories Stored Safely
+        </h2>
         <p className="text-xl">A place where every word stays with you</p>
         <Link
           href="/login"
