cb40961f223d5cbb366169d99d8cfa6c856bebc1
Pasang24 <pasangtheeng14@gmail.com>
2025-04-12 11:29:59 +0545

    style: add padding to container based on screen size

--- components/Container.tsx
+++ components/Container.tsx
@@ -4,7 +4,10 @@ interface ContainerProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 function Container({ children, className = "", ...props }: ContainerProps) {
   return (
-    <div className={`mx-auto max-w-5xl p-2 ${className}`} {...props}>
+    <div
+      className={`mx-auto max-w-5xl p-2 xs:p-4 sm:p-6 ${className}`}
+      {...props}
+    >
       {children}
     </div>
   );
