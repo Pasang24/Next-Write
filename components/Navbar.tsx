7a3b4df96912e41e4b4b96f308a4bd96b007be82
Pasang24 <pasangtheeng14@gmail.com>
2025-04-12 11:26:46 +0545

    style: make navbar extend full width

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import Container from "./Container";
 import ThemeToggleButton from "./ui/ThemeToggleButton";
 import { SquarePen } from "lucide-react";
 import LogoutButton from "./LogoutButton";
@@ -12,45 +11,43 @@ interface NavbarProps {
 
 function Navbar({ authenticatedRoute = false }: NavbarProps) {
   return (
-    <nav className="border-b">
-      <Container className="flex justify-between items-center">
-        <Link
-          href={authenticatedRoute ? "/blogs" : "/"}
-          className="font-bold text-2xl"
-        >
-          NextWrite
-        </Link>
-        <div className="flex items-center">
-          <ThemeToggleButton />
-          {authenticatedRoute ? (
-            <div className="flex">
-              <Link
-                href="/add-blog"
-                className="flex items-center gap-1 px-2 py-1.5 rounded-sm hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50"
-              >
-                <SquarePen size={18} />
-                <span className="hidden xs:block">Write</span>
-              </Link>
-              <LogoutButton />
-            </div>
-          ) : (
-            <div className="flex">
-              <Link
-                href="/login"
-                className="flex items-center gap-1 p-1 xs:px-2 xs:py-1.5 rounded-sm hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50"
-              >
-                Login
-              </Link>
-              <Link
-                href="/signup"
-                className="flex items-center gap-1 p-1 xs:px-2 xs:py-1.5 rounded-sm hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50"
-              >
-                Signup
-              </Link>
-            </div>
-          )}
-        </div>
-      </Container>
+    <nav className="flex justify-between items-center border-b px-2 xs:px-4 py-4">
+      <Link
+        href={authenticatedRoute ? "/blogs" : "/"}
+        className="font-bold text-2xl"
+      >
+        NextWrite
+      </Link>
+      <div className="flex items-center">
+        <ThemeToggleButton />
+        {authenticatedRoute ? (
+          <div className="flex">
+            <Link
+              href="/add-blog"
+              className="flex items-center gap-1 px-2 py-1.5 rounded-sm hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50"
+            >
+              <SquarePen size={18} />
+              <span className="hidden xs:block">Write</span>
+            </Link>
+            <LogoutButton />
+          </div>
+        ) : (
+          <div className="flex">
+            <Link
+              href="/login"
+              className="flex items-center gap-1 p-1 xs:px-2 xs:py-1.5 rounded-sm hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50"
+            >
+              Login
+            </Link>
+            <Link
+              href="/signup"
+              className="flex items-center gap-1 p-1 xs:px-2 xs:py-1.5 rounded-sm hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50"
+            >
+              Signup
+            </Link>
+          </div>
+        )}
+      </div>
     </nav>
   );
 }
